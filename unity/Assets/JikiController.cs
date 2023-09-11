7f70395cb03be38ad5d5d1e0e6f7577e76cb1a9c
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-11 22:59:40 +0900

    自機の移動できる範囲を限定

--- unity/Assets/JikiController.cs
+++ unity/Assets/JikiController.cs
@@ -6,10 +6,10 @@ public class JikiController:MonoBehaviour{
 	public GameObject PlBulletPrefab;
 	void Update(){
 		float c=4;
-		if(Input.GetKey(KeyCode.DownArrow ))transform.Translate( 0,-c,0);
-		if(Input.GetKey(KeyCode.LeftArrow ))transform.Translate(-c, 0,0);
-		if(Input.GetKey(KeyCode.RightArrow))transform.Translate( c, 0,0);
-		if(Input.GetKey(KeyCode.UpArrow   ))transform.Translate( 0, c,0);
+		if(Input.GetKey(KeyCode.DownArrow )&&transform.position.y> 80)transform.Translate( 0,-c,0);
+		if(Input.GetKey(KeyCode.LeftArrow )&&transform.position.x> 96)transform.Translate(-c, 0,0);
+		if(Input.GetKey(KeyCode.RightArrow)&&transform.position.x<800)transform.Translate( c, 0,0);
+		if(Input.GetKey(KeyCode.UpArrow   )&&transform.position.y<880)transform.Translate( 0, c,0);
 		if(Input.GetKey(KeyCode.Z)){
 			if(t==0)Instantiate(PlBulletPrefab,transform.position,Quaternion.identity);
 			t++;
