7dd4c4baf42c2bcb5d8f00a1ddf5e2093e3ee89e
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-12 13:07:07 +0900

    敵の見た目をランダムに変更するように

--- unity/Assets/EnemyController.cs
+++ unity/Assets/EnemyController.cs
@@ -5,6 +5,7 @@ public class EnemyController:MonoBehaviour{
 	float fallSpeed;
 	void Start(){
 		this.fallSpeed=4+4*Random.value;
+		this.gameObject.GetComponent<SpriteRenderer>().sprite=Resources.LoadAll<Sprite>("enemy")[Random.Range(0,8)];
 	}
 	void Update(){
 		transform.Translate(0,-fallSpeed,0,Space.World);
