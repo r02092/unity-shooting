1e06d5b6cdcfb500576c5e87828dd2e3edb67b5c
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-12 13:51:45 +0900

    爆発のエフェクトを調整

--- unity/Assets/ExplosionController.cs
+++ unity/Assets/ExplosionController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 public class ExplosionController:MonoBehaviour{
 	private float scale=100;
 	void Update(){
-		scale+=2;
+		scale+=200/scale;
 		transform.localScale=new Vector3(scale,scale,1);
-		gameObject.GetComponent<Renderer>().material.SetFloat("_Alpha",1-.003f*scale);
-		if(scale>400)Destroy(gameObject);
+		gameObject.GetComponent<Renderer>().material.SetFloat("_Alpha",1-.004f*scale);
+		if(scale>250)Destroy(gameObject);
 	}
 }
