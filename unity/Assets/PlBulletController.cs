95082eff03828dcf5544cdc15b7a2f7d0145731c
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-12 17:45:06 +0900

    弾が敵を貫通するように

--- unity/Assets/PlBulletController.cs
+++ unity/Assets/PlBulletController.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class PlBulletController:MonoBehaviour{
 	public GameObject explosionPrefab;
+	private int combo;
+	void Start(){
+		combo=1;
+	}
 	void Update(){
 		transform.Translate(0,20,0);
 		if(transform.position.y>960)Destroy(gameObject);
 	}
 	void OnTriggerEnter2D(Collider2D coll){
 		if(coll.gameObject.name!="jiki"){
-			GameObject.Find("GameObject").GetComponent<UIController>().AddScore();
+			GameObject.Find("GameObject").GetComponent<UIController>().AddScore(combo);
 			Instantiate(explosionPrefab,transform.position,Quaternion.identity);
 			Destroy(coll.gameObject);
-			Destroy(gameObject);
+			combo++;
 		}
 	}
 }
--- unity/Assets/UIController.cs
+++ unity/Assets/UIController.cs
@@ -7,8 +7,8 @@ public class UIController:MonoBehaviour{
 	int score=0;
 	GameObject scoreText;
 	[SerializeField] TextMeshProUGUI textMeshProUI;
-	public void AddScore(){
-		this.score+=10;
+	public void AddScore(int combo){
+		this.score+=10*combo;
 	}
 	void Update(){
 		textMeshProUI.text=String.Format("{0:#,0}",score);
