dfe7570269667642dc0d2299b19c8671804b92e9
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-12 17:22:05 +0900

    敵の動きを変更

--- unity/Assets/EnemyController.cs
+++ unity/Assets/EnemyController.cs
@@ -1,14 +1,63 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class EnemyController:MonoBehaviour{
-	float fallSpeed;
+	public int type;
+	public int id;
+	private int time;
 	void Start(){
-		this.fallSpeed=4+4*Random.value;
-		this.gameObject.GetComponent<SpriteRenderer>().sprite=Resources.LoadAll<Sprite>("enemy")[Random.Range(0,8)];
+		time=0;
 	}
 	void Update(){
-		transform.Translate(0,-fallSpeed,0,Space.World);
-		if(transform.position.y<0)Destroy(gameObject);
+		switch(type){
+			case 0:
+				if(time<360){
+					transform.Translate(0,-1,0);
+				}else if(time<1128){
+					if(id!=4){
+						int t0speed=2;
+						int t0step=(128*new int[]{0,1,2,7,0,3,6,5,4}[id]/t0speed+time-360)%(1024/t0speed);
+						if(t0step<256/t0speed){
+							transform.Translate(t0speed,0,0);
+						}else if(t0step<512/t0speed){
+							transform.Translate(0,t0speed,0);
+						}else if(t0step<768/t0speed){
+							transform.Translate(-t0speed,0,0);
+						}else{
+							transform.Translate(0,-t0speed,0);
+						}
+					}
+				}else{
+					transform.Translate(0,1,0);
+					if(transform.position.y>960)Destroy(gameObject);
+				}
+				break;
+			case 1:
+				transform.Translate(1,0,0);
+				if(transform.position.x>880)Destroy(gameObject);
+				break;
+			case 2:
+				if(time<720){
+					transform.Translate(0,-1,0);
+				}else if(time>1024){
+					transform.Translate((id%2)*2-1,0,0);
+				}
+				if(time>1328)Destroy(gameObject);
+				break;
+			case 3:
+				transform.Translate(448+(float)Math.Sin((id+(float)time/64)*Math.PI/6)*128-transform.position.x,-1,0);
+				if(transform.position.y<0)Destroy(gameObject);
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+		}
+		time++;
 	}
 }
--- unity/Assets/EnemyGenerator.cs
+++ unity/Assets/EnemyGenerator.cs
@@ -1,12 +1,29 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using Random=UnityEngine.Random;
 
 public class EnemyGenerator:MonoBehaviour{
 	public GameObject enemyPrefab;
 	void Start(){
-		InvokeRepeating("GenEnemy",1,4);
+		InvokeRepeating("GenEnemy",1,9);
 	}
 	void GenEnemy(){
-		Instantiate(enemyPrefab,new Vector3(64+768*Random.value,960,0),Quaternion.identity);
+		int type=Random.Range(0,4);
+		for(int i=0;i<new int[]{9,12,10,12,0,0,0,0}[type];i++){
+			GameObject obj=Instantiate(enemyPrefab,new []{
+				new Vector2(320+(i%3)*128,960+(i/3)*128),
+				new Vector2(-128*i,480+(float)Math.Sin(Math.PI/6*i)*128),
+				new Vector2(320+(i%2)*256,960+(i/2)*128),
+				new Vector2(448+(float)Math.Sin(Math.PI/6*i)*128,960+128*i),
+				new Vector2(0,0),
+				new Vector2(0,0),
+				new Vector2(0,0),
+				new Vector2(0,0)
+			}[type],Quaternion.identity);
+			obj.GetComponent<SpriteRenderer>().sprite=Resources.LoadAll<Sprite>("enemy")[type];
+			obj.GetComponent<EnemyController>().type=type;
+			obj.GetComponent<EnemyController>().id=i;
+		}
 	}
 }
