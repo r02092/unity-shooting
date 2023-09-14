2109dea0b4a5cd010ea662ef982cc81568870d24
r02092 <87572333+r02092@users.noreply.github.com>
2023-09-15 00:54:30 +0900

    最後の敵にダメージを与えただけでクリアとなるバグを修正

--- unity/Assets/PlBulletController.cs
+++ unity/Assets/PlBulletController.cs
@@ -10,11 +10,11 @@ public class PlBulletController:MonoBehaviour{
 	void OnTriggerEnter2D(Collider2D coll){
 		if(coll.gameObject.name=="enemy(Clone)"){
 			GameObject.Find("GameObject").GetComponent<UIController>().AddScore();
-			if(coll.gameObject.GetComponent<EnemyController>().type==7)GameObject.Find("Clear").GetComponent<UnityEngine.UI.Image>().enabled=true;
 			if(coll.gameObject.GetComponent<EnemyController>().hp--!=0){
 				coll.gameObject.GetComponent<EnemyController>().timeDamage=coll.gameObject.GetComponent<EnemyController>().time;
 				coll.gameObject.GetComponent<SpriteRenderer>().color=new Color(0,0,1);
 			}else{
+				if(coll.gameObject.GetComponent<EnemyController>().type==7)GameObject.Find("Clear").GetComponent<UnityEngine.UI.Image>().enabled=true;
 				Instantiate(explosionPrefab,transform.position,Quaternion.identity);
 				Destroy(coll.gameObject);
 			}
